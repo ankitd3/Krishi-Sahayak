6f15468f711cb8de10da310343c7a39678e50878
ankitd3 <ankit.dalal488@gmail.com>
2018-06-01 23:02:13 +0530

    Solution translation + UI

--- index.php
+++ index.php
@@ -73,7 +73,7 @@ function fetchQuestion($sql,$solved){
             $temp = fetchSolution($fileName,$temp);
           }
           else{
-            $temp = $temp."<br><a href='Solution.php?qid=".$fileName."'>SOLVE ME</a>";
+            $temp = $temp."<br><a href='SolutionE.php?qid=".$fileName."'>SOLVE ME</a>";
           }
           //Attach comments(if any)
           $temp = fetchComments($fileName,$temp);//check if there are any comments for the qid, returns an array with the filenames of comments to be included.
@@ -389,104 +389,80 @@ function makeText($fileName,$userId,$temp,$dir){
 $conn->close();
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
+
+<head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-    <title>KS</title>
-    <link href="imp.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-  </head>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
 
-  <body class="bg-light">
+    <!--custom css-->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,900" rel="stylesheet">
+    <link rel="stylesheet" href="css/something.css">
+</head>
 
-    <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
-      <a class="navbar-brand mr-auto mr-lg-0" href="#">Krishi Sahayak</a><div id="google_translate_element">hi</div>
-      <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+<body>
+<nav class="navbar navbar-expand-md">
+        <a href="#" style="color: #FAFEF9" class="navbar-brand"> Q/A App for farmers </a>
+        <div id="google_translate_element"></div>
+        <button type="button" data-target="#menu" data-toggle="collapse" aria-controls="menu" aria-expanded="false" aria-label="toggle navigation"
+            class="navbar-toggler navbar-dark">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="menu">
 
-      <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Questions <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Notifications</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Friends <span class="badge badge-pill bg-light align-text-bottom">27</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Profile</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Switch account</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">LANGUAGE</a>
-            <div class="dropdown-menu" aria-labelledby="dropdown01">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </div>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Ask question</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Frequently asked</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">My questions</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Signout</a>
+                </li>
+            </ul>
+        </div>
     </nav>
 
-    <div class="nav-scroller bg-white box-shadow">
-      <nav class="nav nav-underline">
-        <a class="nav-link active" href="#">TAGS</a>
-        
-        <a class="nav-link" href="#">Crops</a>
-        <a class="nav-link" href="#">Soil</a>
-        <a class="nav-link" href="#">Weather</a>
-        <a class="nav-link" href="#">Irrigation</a>
-        <a class="nav-link" href="#">Fertilizers</a>
-        <a class="nav-link" href="#">Loans</a>
-        <a class="nav-link" href="#">Seeds</a>
-      </nav>
+    <div class="container text-center">
+        <h1>Krishi Sahayak</h1>
     </div>
+    <hr>
 
-    <main role="main" class="container">
-      <div class="d-flex align-items-center p-3 my-3 text-white-50 bg-purple rounded box-shadow">
-        <img class="mr-3" src="question.png" alt="" width="48" height="48">
-        <div class="lh-100">
-          <h6 class="mb-0 text-white lh-100">ASK QUESTIONS</h6>
-          <small>VOICE NOTES - TEXT - IMAGES</small>
-        </div>
-      </div>
+    <!--yo-->
 
-      <div class="my-3 p-3 bg-white rounded box-shadow">
-        <h6 class="border-bottom border-gray pb-2 mb-0">SOLVED</h6>
+    <div class="container">
+      
+      <div class="card">
+            <div class="card-header text-white">
+                Solved 
+            </div>
 
         <?php echo $solved;?>
 
-        <small class="d-block text-right mt-3">
-          <a href="#">All updates</a>
-        </small>
       </div>
 
-      <div class="my-3 p-3 bg-white rounded box-shadow">
-        <h6 class="border-bottom border-gray pb-2 mb-0">UNSOLVED</h6>
-        
 
+
+      <div class="card">
+            <div class="card-header text-white">
+                Unsolved 
+            </div>
+        
         <?php echo $unsolved;?>
 
-
-        <small class="d-block text-right mt-3">
-          <a href="#">All suggestions</a>
-        </small>
       </div>
 
       <!--COMMENTS BEAUTIFY IT LIKE IN testOption.html-->
@@ -553,6 +529,12 @@ $conn->close();
     </script>
 
     <script src="http://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
 
   </body>
 </html>
